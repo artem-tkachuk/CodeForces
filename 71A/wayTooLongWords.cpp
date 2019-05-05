82afdc5dcd1874aa8ab6f7b99367656d8a3eb00a
Artem Tkachuk <artemtkachuk@yahoo.com>
2019-05-05 01:51:29 -0700

    Minor changes

--- 71A/wayTooLongWords.cpp
+++ 71A/wayTooLongWords.cpp
@@ -15,7 +15,7 @@ int main() {
 		cin >> word;
 		len = word.length();
 
-		if (len > 10) {
+		if (len > 10) { //check whether the word is "long" or "short"
 			cout << word[0] << len - 2 << word[len - 1] << endl;
 		} else {
 			cout << word << endl;
